f44aaf3a0973ce8fa225653f924d6a50725a54cd
Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>
2023-02-03 16:53:05 +0300

    Add tests for `Next` method
    Signed-off-by: Gökhan Özeloğlu <gokhan.ozeloglu@deliveryhero.com>

--- iterator_test.go
+++ iterator_test.go
@@ -33,7 +33,7 @@ func TestIter_HasNext(t *testing.T) {
 		{
 			name:     "Non-empty iterator, last index",
 			elements: []any{1, 2, 3, 4},
-			idx:      2,
+			idx:      2, // TODO Check this part
 			hasNext:  true,
 		},
 		{
@@ -50,6 +50,61 @@ func TestIter_HasNext(t *testing.T) {
 			ok := it.HasNext()
 			if tc.hasNext != ok {
 				t.Fatalf("expected: %v\nactual: %v\n", tc.hasNext, ok)
+			}
+		})
+	}
+}
+
+func TestIter_Next(t *testing.T) {
+	testCases := []struct {
+		name     string
+		elements []any
+		idx      int
+		expVal   any
+	}{
+		{
+			name:     "Empty iterator",
+			elements: []any{},
+			expVal:   nil,
+		},
+		{
+			name:     "Non-empty iterator, -1 index",
+			elements: []any{1, 2, 3, 4, 5},
+			idx:      -1,
+			expVal:   1,
+		},
+		{
+			name:     "Non-empty iterator, fresh index",
+			elements: []any{1, 2, 3, 4, 5},
+			expVal:   1,
+		},
+		{
+			name:     "Non-empty iterator, non-zero index",
+			elements: []any{1, 2, 3, 4, 5},
+			idx:      2,
+			expVal:   3,
+		},
+		{
+			name:     "Non-empty iterator, last index",
+			elements: []any{1, 2, 3, 4, 5},
+			idx:      4,
+			expVal:   5,
+		},
+		{
+			name:     "Non-empty iterator, out of index",
+			elements: []any{1, 2, 3, 4, 5},
+			idx:      5,
+			expVal:   nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			it := New(tc.elements...)
+			it.idx = tc.idx
+			v := it.Next()
+			if tc.expVal != v {
+				t.Fatalf("expected: %v\nactual: %v\n", tc.expVal, v)
 			}
 		})
 	}
